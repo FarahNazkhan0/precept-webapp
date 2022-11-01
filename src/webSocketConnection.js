a303e5a473e7e15ffec479b736155e6987974d17
FarahNazkhan0 <113188383+FarahNazkhan0@users.noreply.github.com>
2022-11-01 16:50:17 +0500

    Update webSocketConnection.js

--- src/webSocketConnection.js
+++ src/webSocketConnection.js
@@ -5,8 +5,8 @@ const WebSocketConnection = (username, password) => {
 
     document.cookie = username+password
     
-    const client = new W3CWebSocket("wss://workbook.precept.cloud", ["A71IkUY5fVVgNBz1sXxMoI4MjzCYEHzp", username, password])
-    // const client = new W3CWebSocket("ws://localhost:8080")
+//     const client = new W3CWebSocket("wss://workbook.precept.cloud", ["A71IkUY5fVVgNBz1sXxMoI4MjzCYEHzp", username, password])
+    const client = new W3CWebSocket("ws://localhost:8080")
 
     client.onerror = (errEvent) => {
       alert("Error occurred "+errEvent.message);
